5f9c8232b74efed3e5dda38f33c33c92b464f9a3
tiwanari <tatsuyaw0c@gmail.com>
2015-08-03 17:01:29 +0900

    insert debug funtion

--- ice/ForestFire.java
+++ ice/ForestFire.java
@@ -12,10 +12,14 @@ import java.util.*;
 		ObserverSwarm observerSwarm;
 		
 		Globals.env.initSwarm("forest_fire", "0.1", "Iba Lab.", args);
-		observerSwarm=new ObserverSwarm(Globals.env.globalZone);
+		observerSwarm = new ObserverSwarm(Globals.env.globalZone);
+        System.out.println("buildObjects");
 		observerSwarm.buildObjects();
+        System.out.println("buildActions");
 		observerSwarm.buildActions();
+        System.out.println("activateIn");
 		observerSwarm.activateIn(null);
+        System.out.println("go");
 		observerSwarm.go();
 	}
 }
